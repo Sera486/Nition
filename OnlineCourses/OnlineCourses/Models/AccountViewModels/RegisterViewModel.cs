c0a276d5d086b52aac4685af73154f46b491a2ae
seradug@gmail.com <BZsJoznql>
2017-05-25 00:22:32 +0300

    Added data validation

--- OnlineCourses/OnlineCourses/Models/AccountViewModels/RegisterViewModel.cs
+++ OnlineCourses/OnlineCourses/Models/AccountViewModels/RegisterViewModel.cs
@@ -1,42 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace OnlineCourses.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        //[Required]
-        //[EmailAddress]
+        [Required(ErrorMessage = "Email field required")]
+        [EmailAddress(ErrorMessage = "String is not a valid email address")]
         [JsonProperty("email")]
         public string Email { get; set; }
 
-        //[Required]
-        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password field required")]
         [JsonProperty("password")]
         public string Password { get; set; }
 
-        //[DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password confirmation field required")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         [JsonProperty("confirmPassword")]
-        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-        //[Required]
+        [Required(ErrorMessage = "Login field required")]
         [JsonProperty("login")]
         public string Login { get; set; }
 
-        //[Required]
+        [Required(ErrorMessage = "First name field required")]
         [JsonProperty("firstName")]
         public string FirstName { get; set; }
 
-        //[Required]
+        [Required(ErrorMessage = "Last name field required")]
         [JsonProperty("lastName")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Role is not chosen")]
         [JsonProperty("role")]
         public string Role { get; set; }
     }
---
+++ OnlineCourses/OnlineCourses/Models/Validation/ValidateModelAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OnlineCourses.Models.Validation
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new ValidationFailedResult(context.ModelState);
+            }
+        }
+    }
+}
