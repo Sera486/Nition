fc07df1888cfb768db27c48a46413603ec313076
seradug@gmail.com <BZsJoznql>
2017-05-30 03:47:42 +0300

    Added media upload support

---
+++ OnlineCourses/OnlineCourses/Views/Media/CourseImageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace OnlineCourses.Views.Media
+{
+    public class CourseImageViewModel
+    {
+        public int CourseID { get; set; }
+        public IFormFile UploadedFile {get; set; }
+    }
+}
